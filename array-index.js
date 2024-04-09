ac11470503702e8a70188e59527ae99f55c4350b
miftahujannat <miftahurimny@gmail.com>
2024-04-09 09:49:48 +0600

    added

---
+++ array-index.js
@@ -0,0 +1,22 @@
+var books = ['habul','bolod','pagol','battery','napa','sibia'];
+
+var batteryindex = books.indexOf('battery');
+// console.log(batteryindex);
+
+var numbers = [45,98,50,55,70,65];
+var index = numbers.indexOf(43);
+var index = numbers.indexOf(159)
+// console.log(index);
+
+var secondindex = books[9];
+// console.log(secondindex);
+
+// console.log(numbers);
+// numbers[1] = 95;
+numbers[1] = 95;
+numbers[4] = 25;
+// console.log(numbers);
+
+var  names = ['amir','samira','ama','sabiha'];
+names[2] = 'rimni'
+console.log(names);
