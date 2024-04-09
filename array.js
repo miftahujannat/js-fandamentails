7ee3587fa35cef42fee81b3e788ea4680a05f9a5
miftahujannat <miftahurimny@gmail.com>
2024-04-09 08:44:52 +0600

    added

---
+++ array.js
@@ -0,0 +1,15 @@
+var friendsage = [11,15,18];
+
+var murichanachurfee = [34,50,250,20];
+
+var naika = ['mahi', 'opu', 'sabila' ,'kopula'];
+
+var oddnumber = [1,4,5,7,8,9];
+
+var vowels = ['a','e','i','o','u' ];
+
+// console.log(friendsage);
+
+// console.log(naika.length);
+var vowelscount = vowels.length;
+console.log(vowelscount);
--- variable.js
+++ variable.js
@@ -1,12 +1,12 @@
-// number 
+// number type
 var watchprice = 120;
 watchprice = 125;
 
-// string 
+// string type
 var username = 'sabila';
 
-// boolean 
+// boolean type
 var isexpensive = true;
 
-// integer:1 2 3 4 5 6 666 
-// float 2.714 5.321 2.3 
+// integer type:1 2 3 4 5 6 666 
+// float type: 2.714 5.321 2.3 
