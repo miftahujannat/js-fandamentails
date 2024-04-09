d07b7944f74fcceb07b3e537667adf3858c47599
miftahujannat <miftahurimny@gmail.com>
2024-04-09 08:02:14 +0600

    added

---
+++ variable.js
@@ -0,0 +1,12 @@
+// number 
+var watchprice = 120;
+watchprice = 125;
+
+// string 
+var username = 'sabila';
+
+// boolean 
+var isexpensive = true;
+
+// integer:1 2 3 4 5 6 666 
+// float 2.714 5.321 2.3 
