d8756fcf8ae21721f6f9ce32bc52fe60209bd4e8
miftahujannat <miftahurimny@gmail.com>
2024-04-16 21:12:24 +0600

    added

---
+++ comparison.js
@@ -0,0 +1,14 @@
+// console.log( 5 > 6);
+// console.log( 5 < 6);
+// console.log(5 == 6);
+// console.log( 6 == 6);
+// console.log( 6!=6);
+
+
+var  mylove =50;
+var yourlove =100;
+console.log(mylove == yourlove);
+console.log(mylove < yourlove);
+console.log(mylove > yourlove);
+console.log(mylove != yourlove);
+
---
+++ push-pop.js
@@ -0,0 +1,16 @@
+var lasbench = ['kalam','balam','salam'];
+lasbench.push('jalam');
+lasbench.push('palam');
+lasbench.push('nalam');
+// console.log(lasbench);
+
+var friendsage = [11,13,17,18];
+
+// friendsage.push(15);
+console.log(friendsage);
+
+// remove an element fron an array 
+var lastitem = friendsage.pop();
+// friendsage.pop();
+console.log(friendsage);
+console.log(lastitem);
