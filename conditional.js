65b1b6e32b37093aebbe02b171c8099dfc0fbe56
miftahujannat <miftahurimny@gmail.com>
2024-04-21 19:59:47 +0600

    added

---
+++ conditional.js
@@ -0,0 +1,24 @@
+var isfoodready = false;
+// jodi food ready hoi tahole ami khabo 
+// if food is ready i will eat
+// if food is ready == true 
+// if (condition) {
+
+// } 
+
+if (isfoodready == true){
+    console.log('I will eat now')
+}
+
+var iphoneprice = 70000;
+var mybudget =150000;
+
+//  if (iphoneprice < mybudget){
+//      console.log('Iphone diye cute pic tulbo')
+//  }
+
+// if (iphoneprice > mybudget){
+//     console.log('my shuami is best');
+// }
+
+
