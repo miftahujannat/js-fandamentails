17a5c2e79c0b45e8ca1251d1db797f3dfb9b4c8e
miftahujannat <miftahurimny@gmail.com>
2024-04-22 05:53:43 +0600

    added

---
+++ maltiple-condition.js
@@ -0,0 +1,17 @@
+var gotjob = false;
+var savemoney = 250000;
+var hasflat = false;
+var hashouse = true;
+
+// if (gotjob == true && savemoney > 200000){
+//     console.log('cholo biye kore feli');
+// }
+// else{
+//     console.log('tor kopal e biye nai');
+// }
+if (gotjob == true || savemoney > 200000){
+    console.log('cholo biye kore feli');
+}
+else{
+    console.log('tor kopal e biye nai');
+}
