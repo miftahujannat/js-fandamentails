6411ca689f7d3680f304e5c127c7634148638234
miftahujannat <miftahurimny@gmail.com>
2024-04-22 06:41:39 +0600

    added

--- nested-condition.js
+++ nested-condition.js
@@ -2,17 +2,26 @@ var danishprice = 50;
 var butterbreadprice = 25;
 var toastbiscuitprice = 10;
 var mybudget = 5;
+var packewell = true;
 
-if (danishprice < mybudget){
-    console.log('danish khabo danish jabo');
-}
+// if (danishprice < mybudget){
+//     console.log('danish khabo danish jabo');
+// }
 
-else if (butterbreadprice < mybudget){
-    console.log('butterbon diye cha khamu');
+// else if (butterbreadprice < mybudget){
+//     console.log('butterbon diye cha khamu');
+// }
+// else if (toastbiscuitprice < mybudget){
+//     console.log('toste diye cha khamu');
+// }
+// else{
+//     console.log('batasha diye cha khabo');
+// }
+
+if (danishprice < mybudget) {
+    if (packewell == true)
+    console.log('danish khabo');
 }
-else if (toastbiscuitprice < mybudget){
-    console.log('toste diye cha khamu');
-}
-else{
-    console.log('batasha diye cha khabo');
+else {
+    console.log('danish khabo nah amk boka paiso');
 }
