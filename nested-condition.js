5595c06ad3a0f1ebac34b825099114be144a7af3
miftahujannat <miftahurimny@gmail.com>
2024-04-22 06:26:37 +0600

    added

---
+++ nested-condition.js
@@ -0,0 +1,18 @@
+var danishprice = 50;
+var butterbreadprice = 25;
+var toastbiscuitprice = 10;
+var mybudget = 5;
+
+if (danishprice < mybudget){
+    console.log('danish khabo danish jabo');
+}
+
+else if (butterbreadprice < mybudget){
+    console.log('butterbon diye cha khamu');
+}
+else if (toastbiscuitprice < mybudget){
+    console.log('toste diye cha khamu');
+}
+else{
+    console.log('batasha diye cha khabo');
+}
